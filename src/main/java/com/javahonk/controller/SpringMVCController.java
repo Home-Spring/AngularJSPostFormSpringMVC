6de03ef4ab60558a179209728e4f6cb887883b53
dn200978lak <dn200978lak@gmail.com>
2015-03-12 14:27:01 +0200

    0.0.0

---
+++ src/main/java/com/javahonk/controller/SpringMVCController.java
@@ -0,0 +1,21 @@
+package com.javahonk.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class SpringMVCController {
+	
+	@RequestMapping(value = "/PostFormDataJSONArray", method = RequestMethod.POST)
+	public @ResponseBody List<Person> PostFormDataJSONArray(@RequestBody List<Person> person) {	
+		
+		return person;
+	}
+
+}
